c6630bc7f87f835147a5388f562c01804211864d
Ricardo Cutz <ricardcutzh@gmail.com>
2019-10-02 15:43:19 -0600

    agregando prueba

--- redis/recipes/slaves.rb
+++ redis/recipes/slaves.rb
@@ -1,5 +1,5 @@
 master_instance = search("aws_opworks_instance", "self:true").first
-
+layer_id = master_instance["layer_ids"][0]
 search("aws_opsworks_instance", "layer_ids:#{layer_id}").each do |instance|
     puts instance['hostname']
 end
