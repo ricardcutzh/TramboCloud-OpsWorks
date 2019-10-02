ac4c3cdf428989fafd3009bd29f5ab9e07c2dee7
Ricardo Cutz <ricardcutzh@gmail.com>
2019-10-02 16:41:00 -0600

    pruueba de nombre

--- redis/recipes/slaves.rb
+++ redis/recipes/slaves.rb
@@ -1,8 +1,1 @@
-package 'awscli'
-require "aws-sdk-opsworks"
-
-master_instance = search("aws_opworks_instance", "self:true").first
-layer_id = master_instance["layer_ids"][0]
-search("aws_opsworks_instance", "layer_ids:#{layer_id}").each do |instance|
-    puts instance['hostname']
-end
+puts node['hostname']
